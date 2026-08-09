0346d867e5b3b6df096610cd7cc0830b7780e626
nasirmasud <nasir.masud@ymail.com>
2026-08-09 23:53:55 +0600

    feat(client): add getErrorMessage helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { AxiosError } from "axios"
+import type { ErrorResponse } from "@/types/api.types"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getErrorMessage(error: unknown): string {
+  const axiosError = error as AxiosError<ErrorResponse>
+  return axiosError.response?.data?.message ?? "Something went wrong"
 }
