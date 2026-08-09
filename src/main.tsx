440e1da03ed9bf24ca67791e049fd35175398707
nasirmasud <nasir.masud@ymail.com>
2026-08-10 00:00:25 +0600

    feat(client): add next-themes provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "next-themes"
 import "./index.css"
 import { router } from "./routes/router"
 import { useAuthStore } from "./store/authStore"
@@ -19,8 +20,15 @@ useAuthStore.getState().hydrate()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>
 )
